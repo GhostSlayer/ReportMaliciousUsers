f65df11040eb22a1d0a58e37a0f5247c173bc0e6
Slayer <aksuplayz1337@gmail.com>
2021-03-30 20:10:12 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const requestIp = require('request-ip');
+const fetch = require('node-fetch');
+
+let ports = [443, 80];
+
+const app = express();
+
+app.use('/', async (req, res) => {
+    const clientIp = requestIp.getClientIp(req);
+
+    if (clientIp === '91.156.42.96') {
+        return res.redirect('192.168.1.1')
+    }
+
+    await fetch(`https://api.abuseipdb.com/api/v2/report?ip=${clientIp}&comment=Trying to access port 80 or 443&categories=14`, {
+        method: 'post',
+        headers: { 'Key': process.env.ABUSEIPDB_API_KEY }
+    })
+
+    res.sendStatus(404)
+})
+
+
+ports.forEach(function(port) {
+    app.listen(port);
+});
