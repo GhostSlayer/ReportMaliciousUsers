b88d58792613cd69ca9a8cef302289593fd482ba
Slayer <aksuplayz1337@gmail.com>
2021-03-30 20:18:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use('/', async (req, res) => {
     const clientIp = requestIp.getClientIp(req).replace('::ffff:', '');
 
     if (clientIp === '91.156.42.96') {
-        res.redirect('192.168.1.1')
+        res.redirect('http://192.168.1.1')
+        return;
     }
 
     await fetch(`https://api.abuseipdb.com/api/v2/report?ip=${clientIp}&comment=Trying to access port 80 or 443&categories=14`, {
